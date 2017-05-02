4beae68c97e219d39073664a640f38e2178fa39a
paigesimunovich <simup153@newschool.edu>
2017-05-02 15:24:54 -0400

    Create script-1.js

---
+++ script-1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+
+$(document). ready (function() {
+  $("#submit").click(function() {
+    $(this).hide();
+    $("h4").text("BUY TICKETS ONLINE!");
+  });
+});
